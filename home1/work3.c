271b4b3744c84a205d2ad7e6af095bba2b5940bf
guwenjun <guwenjun2017@gmail.com>
2017-10-23 23:43:08 +0800

    better

--- home1/work3.c
+++ home1/work3.c
@@ -1,3 +1,8 @@
+/* *========* *** *============================================== 
+  * 联络信箱: *** :guwenjun2017@gmail.com
+  * 文档用途: *** :CII实验报告
+  * 修订时间: *** *2017年第41周 10月10日 星期二 下午03:36 (283天
+ * *========* *** *============================================== */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -5,7 +10,6 @@ void oneCircle(int *ptr_arr, int size, int add);
 
 int main(int argc, char **argv)
 {
-    int j = 1;
     int size;
     int *ptr_arr = (int *)malloc(sizeof(int) * size);
     int *Ptr_arr = ptr_arr;
@@ -13,34 +17,23 @@ int main(int argc, char **argv)
     int add;
     char character;
 
-    printf("请输入内存的大小(类型为int):");
-    scanf("%d", &size);
-    printf("请输入初始值:");
-    scanf("%d", &num);
-    printf("请输入每次的增量:");
-    scanf("%d", &add);
-    *ptr_arr = num;
-    printf("ptr_arr fv: %d,*ptr_arr fa: %p\n", *ptr_arr, ptr_arr);
-    oneCircle(ptr_arr, size, add);
-    while(j == 1) {
+    while(1){
+	printf("请输入内存的大小(类型为int):");
+	scanf("%d", &size);
+	printf("请输入初始值:");
+	scanf("%d", &num);
+	printf("请输入每次的增量:");
+	scanf("%d", &add);
+	*ptr_arr = num;
+	printf("ptr_arr fv: %d,*ptr_arr fa: %p", *ptr_arr, ptr_arr);
 	printf("\n");
+	oneCircle(ptr_arr, size, add);
+
 	printf("Continue ?[输入y继续n退出]");
 	getchar();
 	scanf("%c", &character);
-	if (character != 'y')
+	if( character != 'y')
 	    break;
-	else {
-	    printf("请输入内存的大小(类型为int):");
-	    scanf("%d", &size);
-	    printf("请输入初始值:");
-	    scanf("%d", &num);
-	    printf("请输入每次的增量:");
-	    scanf("%d", &add);
-	    *ptr_arr = num;
-	    printf("ptr_arr fv: %d,*ptr_arr fa: %p", *ptr_arr, ptr_arr);
-	    printf("\n");
-	    oneCircle(ptr_arr, size, add);
-	}
     }
 
     free(ptr_arr);
@@ -52,3 +45,33 @@ void oneCircle(int *ptr_arr, int size, int add) {
 	*ptr_arr += add;	
     printf("ptr_arr lv: %d,*ptr_arr la: %p\n", *ptr_arr, ptr_arr);
 }
+
+//  printf("请输入内存的大小(类型为int):");
+//  scanf("%d", &size);
+//  printf("请输入初始值:");
+//  scanf("%d", &num);
+//  printf("请输入每次的增量:");
+//  scanf("%d", &add);
+//  *ptr_arr = num;
+//  printf("ptr_arr fv: %d,*ptr_arr fa: %p\n", *ptr_arr, ptr_arr);
+//  oneCircle(ptr_arr, size, add);
+//  while(j == 1) {
+//      printf("\n");
+//      printf("Continue ?[输入y继续n退出]");
+//      getchar();
+//      scanf("%c", &character);
+//      if (character != 'y')
+//          break;
+//      else {
+//          printf("请输入内存的大小(类型为int):");
+//          scanf("%d", &size);
+//          printf("请输入初始值:");
+//          scanf("%d", &num);
+//          printf("请输入每次的增量:");
+//          scanf("%d", &add);
+//          *ptr_arr = num;
+//          printf("ptr_arr fv: %d,*ptr_arr fa: %p", *ptr_arr, ptr_arr);
+//          printf("\n");
+//          oneCircle(ptr_arr, size, add);
+//      }
+//  }
